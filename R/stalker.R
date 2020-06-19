087ab7548309ca03983bb7bfb15d4e4c8ec53771
A-Farina <andrew.g.farina@gmail.com>
2020-06-18 21:15:51 -0400

    Updated description file and dataset documentation

---
+++ R/stalker.R
@@ -0,0 +1,13 @@
+#' Toy dataset of Therapeutic intervention for stalkers.
+#'
+#' A dataset containing the therapy type, amount of therapy, and stalking time in hours.
+#'
+#' @format A data frame with 78 rows and 3 variables:
+#' \describe{
+#'   \item{Therapy}{Therapy Type: Cruel to be Kind Therapy, Psychodyshamic Therapy, None}
+#'   \item{AmtTher}{Amount of Therapy: 6 Weeks, 12 Weeks}
+#'   \item{Stalker}{Time Spent Stalking: Hours per Week}
+#'   ...
+#' }
+#' @source internally created toy dataset
+"stalker"
